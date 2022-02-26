2b47d43540198eae5758f612d0e69fb10790d903
sophie <sophie.kim@luxrobo.com>
2022-02-27 02:23:41 +0900

    Use FlyControls

--- src/three/example15.ts
+++ src/three/example15.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+import { FlyControls } from 'three/examples/jsm/controls/FlyControls';
 
 /**
  * 카메라 컨트롤 - OrbitControls, TrackballControls
@@ -44,10 +45,17 @@ export function initThreejs(canvas: HTMLCanvasElement | null) {
 
   // 2. TrackballControls
   // 360도 회전 가능, 기본으로 enableDamping 설정됨, update 안 해주면 안 됨
-  const controls = new TrackballControls(camera, renderer.domElement);
-  controls.maxDistance = 20;
-  controls.minDistance = 5;
+  // const controls = new TrackballControls(camera, renderer.domElement);
+  // controls.maxDistance = 20;
+  // controls.minDistance = 5;
   // controls.target.set(3, 3, 3);
+
+  // 3. FlyControls
+  // 키보드 wasd로 방향키처럼 조작, 좌클릭 앞으로 우클릭 뒤로, r위로 f아래로, q시계 e 반시계방향 회전 가능
+  const controls = new FlyControls(camera, renderer.domElement);
+  controls.rollSpeed = 0.1; // 마우스 따라가는 속도
+  // controls.movementSpeed = 3; // 앞뒤좌우 움직임 속도
+  // controls.dragToLook = true; // 마우스에 반응하지 않고 드래그에만 반응
 
   // Mesh
   const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -69,8 +77,11 @@ export function initThreejs(canvas: HTMLCanvasElement | null) {
     scene.add(mesh);
   }
 
+  const clock = new THREE.Clock();
+
   function draw() {
-    controls.update();
+    const delta = clock.getDelta();
+    controls.update(delta); // FlyControls는 delta 인자 필요 - 마우스 위치를 따라 회전
 
     renderer.render(scene, camera);
     renderer.setAnimationLoop(draw);
