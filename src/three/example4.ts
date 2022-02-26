7d92089c14804fd4785106e8c02efd5bbced7cb6
sophie <sophie.kim@luxrobo.com>
2022-02-26 17:30:29 +0900

    Create file: setting light

---
+++ src/three/example4.ts
@@ -0,0 +1,54 @@
+import * as THREE from 'three';
+
+// 조명 설정
+export function initThreejs(canvas: HTMLCanvasElement | null) {
+  if (!canvas) return;
+
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('blue');
+
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  camera.position.x = 1;
+  camera.position.y = 2;
+  camera.position.z = 5;
+  scene.add(camera);
+
+  /**
+   * Light
+   */
+  // DirectionalLight 태양빛과 비슷한 조명
+  const light = new THREE.DirectionalLight(0xffffff, 1); // 빛의 색상, 강도
+  // 기본적으로 위쪽에서 아래로 비춤
+  light.position.x = 1;
+  light.position.z = 2;
+  scene.add(light);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // MeshBasicMaterial은 빛에 반응하지 않는, 빛 없어도 보이는 재질
+  // const material = new THREE.MeshBasicMaterial({
+  //   color: 'red',
+  // });
+  const material = new THREE.MeshStandardMaterial({
+    color: 'red',
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer.render(scene, camera);
+
+  function resizeCanvas() {
+    // 카메라
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix(); // 카메라 투영에 관련된 값에 변화가 있을 경우 실행
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener('resize', resizeCanvas);
+}
