4c8373463f2a013ad71c1c997f815227d0f7e028
sophie <sophie.kim@luxrobo.com>
2022-02-27 14:09:42 +0900

    Create directory

--- src/three/main.ts
+++ src/three/main.ts
@@ -1,3 +1,18 @@
-import { initThreejs } from './example1';
+// import { initThreejs } from './basic/example1';
+// import { initThreejs } from './basic/example2';
+// import { initThreejs } from './basic/example3';
+// import { initThreejs } from './basic/example4';
+// import { initThreejs } from './basic/example5';
+// import { initThreejs } from './basic/example6';
+// import { initThreejs } from './basic/example7';
+// import { initThreejs } from './basic/example8';
+// import { initThreejs } from './tool/example9';
+// import { initThreejs } from './tool/example10';
+// import { initThreejs } from './tool/example11';
+// import { initThreejs } from './transform/example12';
+// import { initThreejs } from './transform/example13';
+// import { initThreejs } from './geometry/example14';
+// import { initThreejs } from './camera-control/example15';
+import { initThreejs } from './camera-control/example16';
 
 export { initThreejs };
