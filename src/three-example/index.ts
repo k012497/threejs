c17dbe9f715b8a204b3f977840f776391937ff9d
sophie <sophie.kim@luxrobo.com>
2022-03-01 01:29:17 +0900

    import examples

--- src/three-example/index.ts
+++ src/three-example/index.ts
@@ -19,6 +19,26 @@
 // import { initThreejs } from './camera-control/example1';
 // import { initThreejs } from './camera-control/example2';
 
-import { initThreejs } from './material/example1';
+// import { initThreejs } from './material/example1';
+// import { initThreejs } from './material/example2';
+// import { initThreejs } from './material/example3';
+// import { initThreejs } from './material/example4';
+// import { initThreejs } from './material/example5';
+// import { initThreejs } from './material/example6';
+// import { initThreejs } from './material/example7';
+// import { initThreejs } from './material/example8';
+// import { initThreejs } from './material/example9';
+// import { initThreejs } from './material/example10';
+// import { initThreejs } from './material/example11';
+// import { initThreejs } from './material/example12';
+// import { initThreejs } from './material/example13';
+
+// import { initThreejs } from './light/example1';
+// import { initThreejs } from './light/example2';
+// import { initThreejs } from './light/example3';
+// import { initThreejs } from './light/example4';
+// import { initThreejs } from './light/example5';
+// import { initThreejs } from './light/example6';
+import { initThreejs } from './light/example7';
 
 export { initThreejs };
